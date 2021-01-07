13ce4cc2290b3fff54ca4379b53668445d49a87f
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-07 00:56:26 -0600

    Trouble shooting function codes to get boolean statement true and console logging.

--- script.js
+++ script.js
@@ -1,68 +1,94 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText= document.querySelector ("#password");
 
-// Write password to the #password input
+// Write password to the #password input 
   function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword();  
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  } 
+  
 
-}
 
-/*Variables for password
-var lowerCase= "abcdefghijklmnopqrstuvwxyz";
-var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialCharacter= "!@#$%^&*()_+?><:;-";
-var number= "0123456789"; */
-
-// To prompt password length
-var passLength= ""
- /* if ((passLength >= 8) && (passLength <= 128)){
-    alert ("Requirment met.")
-  }
-
-  else {
-    alert (" Password length must be 8-128 characters long!")
-    
-  } */
-
-  //Loop Password prompt until condition is true.
+  //Loop Password length prompt until condition is met.
 
     var passLength = prompt ("Select password length, must enter a number between 8 and 128.")
 
-    while ((passLength <= 8) || (passLength >= 128)) {
+    while ((passLength <= 7) || (passLength >= 129)) {
       alert (" Password length must be a number from 8 to 128!");
       passLength = prompt ("Select password length, must enter a number between 8 and 128.")
     }
-    console.log (passLength);
 
     //Confrim if lower case is included in password.
     var passlCase = confirm ("Do you want to include lower case letters in your password? Select 'Ok' to include or 'Cancel' to not include in password.")
 
-    console.log(passlCase);
-
     // Confirm if upper case is included in password.
     var passuCase = confirm ("Do you want to include upper case letters in your password? Select 'Ok' to include or 'Cancel' to not include in password.")
 
-    console.log(passuCase);
     //Confirm if special character is included in password.
     var passspecialC = confirm ("Do you want to include special characters in your password? Select 'Ok' to include or 'Cancel' to not include in password.")
-    console.log(passspecialC);
 
     //Confirm if numbers are included in password.
     var passnumber = confirm ("Do you want to include numbers in your password? Select 'Ok' to include or 'Cancel' to not include in password.")
-    console.log(passnumber);
 
-    alert ("Click on Generate Button to get your password.")
 
+    alert ("Click on Generate Password button to get your password.")
+ 
+    var length = passLength; 
+    var lowerCase = passlCase;
+    var upperCase = passuCase;
+    var specialCharacter= passspecialC;
+    var number = passnumber;
+
+ 
 
 // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", () => {
+    const charalength = +length;
+    const haslCase = lowerCase; 
+    const hasuCase = upperCase; 
+    const hasspecial = specialCharacter; 
+    const hasnumber = number; 
 
+    passwordText = generatePassword(haslCase, hasuCase, hasspecial, hasnumber);
+
+  });
+
+  
+  //Generate password function.
+    function generatePassword (lowerCase, upperCase, specialCharacter, number) {
+      
+      let generatePassword= "";
+
+      const typeCount = lowerCase + upperCase + specialCharacter + number;
+
+      console.log("typescount", typeCount);
+
+      const typesArr = [{lowerCase}, {upperCase}, {specialCharacter}, {number}];
+
+      console.log("typesArry", typesArr);
+
+    }
+
+
+    //password = generatePassword (
+      //passLength, passlCase, passuCase, passspecialC, passnumber
+    
+
+
+
+ // passwordText.innerText = generatePassword (passLength, passlCase, passuCase, passspecialC, passnumber);
+
+  //Generate password function.
+
+
+ /* function generatePassword( passLength, passlCase, passuCase, passspecialC, passnumber){
+  }
+  */
 
   //Connect all functions into one random
-  const randomPass ={
+  const randomFunc = {
     lower: getlowerCase,
     upper: getupperCase,
     special: getspecialCharacter,
